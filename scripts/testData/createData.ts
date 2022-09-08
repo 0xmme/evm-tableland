e1f4730e61e60055486ac560ee99139c8e59dab5
mme <9083787+mme022@users.noreply.github.com>
2022-09-08 16:46:32 +0100

    added createData script, removed createTable

--- scripts/testData/createData.ts
+++ scripts/testData/createData.ts
@@ -1,7 +1,7 @@
 /* eslint-disable object-shorthand */
 /* eslint-disable no-unused-vars */
 import { ethers, network } from "hardhat";
-import { Wallet } from "ethers";
+import { BigNumber, Wallet } from "ethers";
 import { connect, Connection } from "@tableland/sdk";
 import * as fs from "fs";
 
@@ -9,6 +9,7 @@ async function main() {
   console.log(`\nCreating table on ${network.name}...`);
 
   const netOpKov = "optimism-kovan";
+  // const netOpGoe = "optimism-goerli";
 
   const currentTablesFile: string = "constants/deployedTables.json";
   const tablesCreateFile: string = "constants/tableScheme.json";
@@ -18,6 +19,10 @@ async function main() {
     network.name,
     process.env.OPTIMISM_KOVAN_API_KEY!
   );
+  // const provider = new ethers.providers.AlchemyProvider(
+  //  network.name,
+  //  process.env.OPTIMISM_GOERLI_API_KEY!
+  // );
 
   const account = new Wallet(process.env.ETHEREUM_PRIVATE_KEY!, provider);
 
@@ -26,6 +31,8 @@ async function main() {
     chain: netOpKov,
     signer: account,
   });
+
+  //  ${name} (${tablesCreate.tablesToCreate[i].schema_short}) VALUES (${tablesCreate.tablesToCreate[i].data[j]});`
 
   console.log("*************************************************");
   console.log(
@@ -47,6 +54,49 @@ async function main() {
     console.log("*************************************************");
     console.log(`Table ${name} on ${network.name} created.`);
 
+    console.log("*************************************************");
+    console.log(`Trying to write data into'${name}'`);
+
+    const dataRowsToCreate = tablesCreate.tablesToCreate[i].data.length;
+    const dataColumnsToCreate = tablesCreate.tablesToCreate[i].fieldCount;
+
+    let sqlStatement = "";
+
+    for (let row = 0; row < dataRowsToCreate; row++) {
+      // Building SQL Statement for Tableland
+      sqlStatement = sqlStatement
+        .concat(`INSERT INTO `)
+        .concat(`${name!}`)
+        // .concat(name!)
+        .concat(` (${tablesCreate.tablesToCreate[i].schema_short}) `)
+        .concat(`VALUES (`);
+      for (let column = 0; column < dataColumnsToCreate; column++) {
+        if (
+          typeof tablesCreate.tablesToCreate[i].data[row][column] === "string"
+        ) {
+          sqlStatement = sqlStatement.concat(
+            `'${tablesCreate.tablesToCreate[i].data[row][column]}'`
+          );
+        } else {
+          sqlStatement = sqlStatement.concat(
+            `${tablesCreate.tablesToCreate[i].data[row][column]}`
+          );
+        }
+        if (column < dataColumnsToCreate - 1) {
+          sqlStatement = sqlStatement.concat(`,`);
+        }
+      }
+      sqlStatement = sqlStatement.concat(`);`);
+
+      const { hash: writeHash } = await tableland.write(sqlStatement);
+
+      console.log("*************************************************");
+      console.log(
+        `table ${name} sucessfully filled with data row ${sqlStatement} on tx ${writeHash}`
+      );
+      sqlStatement = "";
+    }
+
     if (txnHash !== null) {
       updateContractAddresses(
         currentTablesFile,
@@ -54,7 +104,8 @@ async function main() {
         name!,
         tablesCreate.tablesToCreate[i].schema,
         chainId,
-        txnHash
+        txnHash,
+        tablesCreate.tablesToCreate[i].data
       );
     }
   }
@@ -66,7 +117,8 @@ const updateContractAddresses = (
   name: string,
   schema: string,
   chainId: number,
-  txnHash: string
+  txnHash: string,
+  data: any[]
 ) => {
   const currentTables = JSON.parse(fs.readFileSync(currentTablesFile, "utf8"));
   currentTables[chainId].push({
@@ -75,6 +127,7 @@ const updateContractAddresses = (
     chainId: chainId,
     txnHash: txnHash,
     schema: schema,
+    data: data,
   });
 
   fs.writeFileSync(currentTablesFile, JSON.stringify(currentTables));
